4b75ee3a011d7e5995bd43ca143aed07c26ad9bf
Maciel B <macielbmo@gmail.com>
2023-07-27 19:21:13 -0300

    Realizei a configuração inicial do meu Router e das paginas do meu site Home e Pedidos. Foi criado também dois componentes que são o Footer e NavBar, onde cada pagina do site receberá esses dois componentes.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,6 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="home"></div>
+  <h1>HOME</h1>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<script></script>
