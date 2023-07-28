c189ad8d9742f0551eee60a9830a8bdbadf84bfc
Maciel B <macielbmo@gmail.com>
2023-07-28 17:53:28 -0300

    Criação do metodo para inserir os pedidos no banco de dados.

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -2,7 +2,7 @@
   <div>
     <p>Componente de Mensagem</p>
     <div>
-      <form id="burger-form">
+      <form id="burger-form" @submit="createBurger">
         <div class="input-container">
           <label for="name">Nome</label>
           <input
@@ -26,7 +26,7 @@
 
         <div class="input-container">
           <label for="carne">Escolha a carne:</label>
-          <select name="carne" id="carne" v-model="pao">
+          <select name="carne" id="carne" v-model="carne">
             <option value="">Seleciona o tipo de carne</option>
             <option v-for="carne in carnes" :key="carne.id" :value="carne.tipo">
               {{ carne.tipo }}
@@ -70,11 +70,10 @@ export default {
       paes: null,
       carnes: null,
       opcionaisdata: null,
-      nome: null,
+      name: null,
       pao: null,
       carne: null,
-      opcional: [],
-      status: "Solicitado",
+      opcionais: [],
       msg: null,
     };
   },
@@ -86,6 +85,38 @@ export default {
       this.paes = data.paes;
       this.carnes = data.carnes;
       this.opcionaisdata = data.opcionais;
+    },
+
+    async createBurger(e) {
+      e.preventDefault();
+
+      const data = {
+        name: this.name,
+        carne: this.carne,
+        pao: this.pao,
+        opcionais: Array.from(this.opcionais),
+        status: "Solicitado",
+      };
+
+      const dataJson = JSON.stringify(data);
+
+      const req = await fetch("http://localhost:3000/burgers", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: dataJson,
+      });
+
+      const res = await req.json();
+
+      // Ainda será adicionado, um mensagem confirmando o pedido
+
+      // limpar mensagem
+
+      // uma função para limpar os campos
+      this.name = "";
+      this.carne = "";
+      this.paes = "";
+      this.opcionais = "";
     },
   },
   mounted() {
